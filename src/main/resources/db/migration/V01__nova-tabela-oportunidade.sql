7a0248263d3dbf2a722b926c90a72973510f9120
Erik <erik.nishikawa@yaman.com.br>
2020-01-30 09:11:27 -0300

    Conectando com banco do heroku

--- src/main/resources/db/migration/V01__nova-tabela-oportunidade.sql
+++ src/main/resources/db/migration/V01__nova-tabela-oportunidade.sql
@@ -1,8 +1,6 @@
 create table oportunidade(
-	id bigint auto_increment not null,
+	id serial primary key,
 	nome_prospecto varchar(80) not null,
 	descricao varchar(200) not null,
-	valor decimal(10,2),
-	
-	primary key (id)
+	valor numeric(10,2)
 );
